a3aaf8b07cd07dfebb093f3eba462e0c6e497124
Sadat Manjur <sadat@mastude44fmq6l4.home>
2022-10-26 11:49:11 +0100

    Added new features

--- bankAccount.js
+++ bankAccount.js
@@ -8,23 +8,10 @@ class BankAccount {
   getToday() {
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, "0");
-    let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    let mm = String(today.getMonth() + 1).padStart(2, "0");
     let yyyy = today.getFullYear();
     today = dd + "/" + mm + "/" + yyyy;
     return today;
-  }
-
-  // deposits money into the account
-  // creates a new debit entry in the bank statement
-  deposit(amount) {
-    this.balance = this.balance + amount;
-    this.transactions.push(this.getToday() + ` || ${amount.toFixed(2)} || || ${this.balance.toFixed(2)}`);
-  }
-
-  // withdraws money from the account
-  // creates a new credit entry in the bank statement
-  withdraw(amount) {
-    this.balance = this.balance - amount;
   }
 
   // displays the current bank balance
@@ -32,12 +19,45 @@ class BankAccount {
     return this.balance;
   }
 
+  // returns the recorded transactions
   getTransactions() {
     return this.transactions;
   }
+
+  // deposits money into the account
+  // creates a new debit entry in the bank statement
+  deposit(amount) {
+    this.balance = this.balance + amount;
+    this.transactions.push(
+      this.getToday() +
+        ` || ${amount.toFixed(2)} || || ${this.balance.toFixed(2)}`
+    );
+  }
+
+  // withdraws money from the account
+  // creates a new credit entry in the bank statement
+  withdraw(amount) {
+    if (amount > this.getBalance()) {
+      return "Insufficient funds in your account.";
+    } else {
+      this.balance = this.balance - amount;
+      this.transactions.push(
+        this.getToday() +
+          ` || || ${amount.toFixed(2)} || ${this.balance.toFixed(2)}`
+      );
+    }
+  }
+
   // returns bank statement with all the transactions
   getStatement() {
-    return "date || credit || debit || balance";
+    if (this.transactions.length === 0) {
+      return "date || credit || debit || balance";
+    } else {
+      return (
+        "date || credit || debit || balance\n" +
+        this.getTransactions().reverse().join("\n")
+      );
+    }
   }
 }
 
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -1,38 +1,68 @@
-const BankAccount = require('./bankAccount');
+const BankAccount = require("./bankAccount");
 
-describe('BankAccount', () => {
-  it('returns the bank balance', () => {
+describe("BankAccount", () => {
+  it("returns the bank balance", () => {
     const bankAccount = new BankAccount();
     expect(bankAccount.getBalance()).toEqual(0);
   });
 
-  it('deposits money into account', () => {
+  it("deposits money into account", () => {
     const bankAccount = new BankAccount();
     bankAccount.deposit(1000);
     expect(bankAccount.getBalance()).toEqual(1000);
   });
 
-  it('returns balance after making a withdrawal', () => {
+  it("returns balance after making a withdrawal", () => {
     const bankAccount = new BankAccount();
     bankAccount.deposit(1000);
     bankAccount.withdraw(500);
     expect(bankAccount.getBalance()).toEqual(500);
   });
 
-  it('returns the bank statement', () => {
+  it("returns the bank statement", () => {
     const bankAccount = new BankAccount();
-    expect(bankAccount.getStatement()).toEqual("date || credit || debit || balance");
+    expect(bankAccount.getStatement()).toEqual(
+      "date || credit || debit || balance"
+    );
   });
 
-  it('gets today\'s date', () => {
+  it("gets today's date", () => {
     const bankAccount = new BankAccount();
-    expect(bankAccount.getToday()).toEqual("25/10/2022");
+    expect(bankAccount.getToday()).toEqual("26/10/2022");
   });
 
-  it ('adds a new entry to transactions when a deposit is made', () => {
+  it("adds a new entry to transactions when a deposit is made", () => {
     const bankAccount = new BankAccount();
     bankAccount.deposit(1000);
-    expect(bankAccount.getTransactions()).toEqual(["25/10/2022 || 1000.00 || || 1000.00"]);
+    expect(bankAccount.getTransactions()).toEqual([
+      "26/10/2022 || 1000.00 || || 1000.00",
+    ]);
   });
 
-})
+  it("adds a new entry to transactions when a withdrawal is made", () => {
+    const bankAccount = new BankAccount();
+    bankAccount.deposit(1000);
+    bankAccount.withdraw(500);
+    expect(bankAccount.getTransactions()).toEqual([
+      "26/10/2022 || 1000.00 || || 1000.00",
+      "26/10/2022 || || 500.00 || 500.00",
+    ]);
+  });
+
+  it("prints the bank statement after 2 transactions are made", () => {
+    const bankAccount = new BankAccount();
+    bankAccount.deposit(1000);
+    bankAccount.withdraw(500);
+    expect(bankAccount.getStatement()).toEqual(
+      "date || credit || debit || balance\n26/10/2022 || || 500.00 || 500.00\n26/10/2022 || 1000.00 || || 1000.00"
+    );
+  });
+
+  it("returns error message when withdrawal amount is greater than account balance", () => {
+    const bankAccount = new BankAccount();
+    bankAccount.deposit(1000);
+    expect(bankAccount.withdraw(1500)).toEqual(
+      "Insufficient funds in your account."
+    );
+  });
+});
